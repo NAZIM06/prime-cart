61879154e1509b0c0ef907bf4f39446327d68698
unknown <anikm2982@gmail.com>
2023-03-28 23:43:39 +0600

    setup done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h2>
+                <span style={{ color: '#7421B3' }}>PRIME</span>
+                <span style={{ color: '#3D4141' }}>CART</span>
+            </h2>
+        </div>
+    );
+};
+
+export default Header;
